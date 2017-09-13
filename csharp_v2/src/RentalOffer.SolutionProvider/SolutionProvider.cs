f98411f731ae799847857a42748cc68f3ff914c3
David Nguyen <hungphuongjc@gmail.com>
2017-09-13 10:47:23 +0800

    implement solution provider for unsatisfied need

---
+++ csharp_v2/src/RentalOffer.SolutionProvider/SolutionProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MicroServiceWorkshop.RapidsRivers;
+using MicroServiceWorkshop.RapidsRivers.RabbitMQ;
+using Newtonsoft.Json.Linq;
+
+namespace RentalOffer.SolutionProvider
+{
+    class SolutionProvider : River.IPacketListener
+    {
+        static void Main(string[] args)
+        {
+            string host = args[0];
+            string port = args[1];
+
+            var rapidsConnection = new RabbitMqRapids("monitor_in_csharp", host, port);
+            var river = new River(rapidsConnection);
+            river.Require("need"); 
+            river.Register(new SolutionProvider());
+        }
+
+        public void ProcessPacket(RapidsConnection connection, JObject jsonPacket, PacketProblems warnings)
+        {
+            Console.WriteLine(" [*] {0}", warnings);
+            var jsonMessage = "{\"solution\":\"" + jsonPacket["need"] + "\"}";
+            Console.WriteLine(" [<] {0}", jsonMessage);
+            connection.Publish(jsonMessage);
+        }
+
+        public void ProcessError(RapidsConnection connection, PacketProblems errors)
+        {
+            Console.WriteLine(" [x] {0}", errors);
+        }
+    }
+}
